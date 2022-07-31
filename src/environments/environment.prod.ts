cfa6b0385b991bdc9f805679805585f3495aa9ce
Vadym Kuzovkin <vadymkuzovkin@gmail.com>
2022-07-31 11:49:12 +0300

    feat: minor changes and import endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,14 +5,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://ez7humm0sc.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://0tldr3qf4h.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: false,
     order: false,
-    import: false,
+    import: true,
     bff: true,
     cart: false,
   },
